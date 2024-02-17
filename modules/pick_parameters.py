869d7e2ae925974bf51a9038b4214307e1573174
ania-m <anna_milczarek@hotmail.com>
2024-02-17 13:25:28 -0500

    broke down main script into modules

---
+++ modules/pick_parameters.py
@@ -0,0 +1,64 @@
+#IronPython
+
+import Autodesk
+from Autodesk.Revit import DB
+from pyrevit import forms
+from System.Collections.Generic import List
+uiapp = __revit__
+uidoc = uiapp.ActiveUIDocument
+doc = uiapp.ActiveUIDocument.Document
+
+def pick_parameters(picked_params=["Number","Level"]):
+    """pick parameters function - accepts array of default parameters 
+
+    Args:
+        picked_params (list, optional): _description_. Defaults to ["Number","Level"].
+    """
+    Label = DB.LabelUtils
+    #Get rooms
+    all_rooms = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Rooms).WhereElementIsNotElementType()
+
+    #Take first room from list of rooms as an example room
+    example_room = [room for room in all_rooms][0]
+
+    #Create dictionary of parameters, grouped by their group (e,g Group1:[par1,par20], Group2:[par3])
+    parameters = {Label.GetLabelForGroup(param.Definition.GetGroupTypeId()):
+                [
+                p.Definition.Name for p in example_room.Parameters
+                if Label.GetLabelForGroup(p.Definition.GetGroupTypeId()) == Label.GetLabelForGroup(param.Definition.GetGroupTypeId())
+                and p.Definition.Name not in picked_params]
+                for param in example_room.Parameters}
+
+    #Sort param dictionary, param arrays and remove duplicate params
+    for group in sorted(parameters):
+            parameters[group].sort()
+            temp = set(parameters[group])
+            parameters[group] = temp
+
+    # for g in parameters:
+    #     print(g)
+    #     for p in parameters[g]:
+    #         print("---->"+p)
+    #     print("___")
+
+    #Create and add an ALL group of parameters that is formated as ALL: ["group: paramter1", "group: paramter2",etc],
+    all_parameters_group = [key+": "+value for key, array in parameters.items() for value in array]
+    all_parameters_group.sort()
+    parameters["ALL"]=all_parameters_group
+
+    #Pick parameters form
+    result = forms.SelectFromList.show(parameters,
+        "Pick Additional Parameters",
+        width = 300,
+        height = 400,
+        button_name='Next', 
+        multiselect = True,
+        group_selector_title='Parameter Groups',
+        )
+
+    if result != None:
+        #remove the group (remove everything before ": ") from each result picked from ALL category
+        result = [res.split(": ",1)[1] if ": " in res else res for res in result]
+        picked_params.extend(result)
+    
+    return(picked_params)
---
+++ modules/send_dict.py
@@ -0,0 +1,22 @@
+#IronPython
+
+import subprocess
+import json
+
+def send_dict(sent_dict, pathToScript):
+    """_summary_
+
+    Args:
+        sent_dict (_type_): _description_
+        pathToScript (_type_): _description_
+    """
+    message = json.dumps(sent_dict)
+    cmd = ['python', pathToScript, message]
+
+    # Create the subprocess startup info object This prevents the black terminal window from popping up when running hte subprocess
+    startup_info = subprocess.STARTUPINFO()
+    startup_info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+    # Run the command and capture the output
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, startupinfo=startup_info)
+
